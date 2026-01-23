279ef78acfccea7425a777d30b9e5bb187bcb908
Eye-Kyu <wankenobi29@gmail.com>
2026-01-24 00:04:04 +0300

    almost sure bet

--- app/components/Services2.tsx
+++ app/components/Services2.tsx
@@ -22,46 +22,46 @@ export default function Services2() {
           </div>
         </div>
         {/*</section>*/}
-        <section className="flex">
+        <section className="flex py-6">
           <div className="w-1/2 pr-4">
             <AisleCard
               title="ELECTRONICS"
               text="Whether you're looking for the latest gadgets or essential household electronics, our selection has something for everyone."
               image="/Images/Hero/hero1.webp"
-              className="pl-8 pr-8"
+              className=""
               reverse
             />
           </div>
           {/* RIGHT TITLE */}
-          <div className="w-1/2 ml-4">
+          <div className="w-1/2 pl-4">
             <AisleCard
               title="GROCERIES"
               text="Fresh from local farms to international delights, our grocery aisle is stocked with everything you need for your kitchen and more."
               image="/Images/Hero/Groceries.webp"
               reverse
-              className="pl-8"
+              className=""
             />
           </div>
         </section>
 
         {/*</section>*/}
-        <section className="flex">
+        <section className="flex py-6">
           <div className="w-1/2 pr-4">
             <AisleCard
               title="HOUSEHOLD ITEMS"
               text="From cleaning supplies to kitchen essentials, our household items aisle has everything you need to keep your home running smoothly."
               image="/Images/Hero/hero1.webp"
-              className="pl-8 pr-8"
+              className=""
             />
           </div>
           {/* RIGHT TITLE */}
-          <div className="w-1/2 ml-4 hidden">
+          <div className="w-1/2 pl-4 hidden">
             <AisleCard
               title="GROCERIES"
               text="Fresh from local farms to international delights, our grocery aisle is stocked with everything you need for your kitchen and more."
               image="/Images/Hero/Groceries.webp"
               reverse
-              className="pl-8"
+              className=""
             />
           </div>
         </section>
